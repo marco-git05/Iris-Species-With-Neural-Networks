d90cf8b8285dab8a9a3dd8e590e0535fa35ed3a3
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-05-14 11:29:45 -0500

    Create neural_network.py and boilerplate

---
+++ neural_network.py
@@ -0,0 +1,21 @@
+class NeuralNetwork:
+    def __init__(input, target, hidden):
+        pass
+
+    def feed_forward(obj):
+        pass
+
+    def backpropigation(obj):
+        pass
+
+    def train(obj):
+        pass
+
+    def activation_function():
+        pass
+
+    def activation_derivative():
+        pass
+
+    
+    
